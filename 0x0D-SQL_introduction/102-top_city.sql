3d005b01da8089f467e7bb740185ba7b698ff124
asnakeassefa <asnakeassefa28@gmail.com>
2022-08-11 22:59:05 +0300

    Top 3 list

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- displaying Top 3 ava tempreture by city 
-SELECT city, avg_temp FROM (SELECT city,AVG(value) AS avg_temp FROM temperatures GROUP BY city) AS T WHERE month IN (7,8) ORDER BY avg_temp DESC;
+SELECT city, avg_temp FROM (SELECT city,AVG(value) AS avg_temp FROM temperatures WHERE month IN (7,8) GROUP BY city) AS T ORDER BY avg_temp DESC LIMIT 3;
