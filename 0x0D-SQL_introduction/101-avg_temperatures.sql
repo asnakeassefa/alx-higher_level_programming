f9ba8bdf07f3f56df6f1df56629c144e3b377d24
asnakeassefa <asnakeassefa28@gmail.com>
2022-08-11 21:44:36 +0300

    tempreture ava

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- displaying ava tempreture by city
-SELECT city,AVA(value) AS avg_temp FROM tempretures GROUP BY value ORDER BY AVA(value) DESC
+SELECT city,AVG(value) AS avg_temp FROM tempretures GROUP BY value ORDER BY AVG(value) DESC
