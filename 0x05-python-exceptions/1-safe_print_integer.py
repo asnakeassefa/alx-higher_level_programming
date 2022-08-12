5307407b02d57fc024f8e27ca7685c609e72f87f
asnakeassefa <asnakeassefa28@gmail.com>
2022-08-12 20:24:25 +0300

    integer identifier

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         "{:d}".format(value)
-        return isinstance(value)
+        return True
     except:
-        return isinstance(value,int)
+        return False
