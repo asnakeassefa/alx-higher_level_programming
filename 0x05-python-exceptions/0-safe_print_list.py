c012f73c975035929a11ba3581e7ff125ba74d32
asnakeassefa <asnakeassefa28@gmail.com>
2022-08-12 16:10:19 +0300

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,6 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0,x):
         try:
             print(my_list[x])
-            
         except IndexError:
             return i
     return i
