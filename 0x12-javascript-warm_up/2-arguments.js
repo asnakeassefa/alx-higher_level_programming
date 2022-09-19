7a96ba4bd82100f774bc11700a6aa39fbcdb6a2a
asnakeassefa <asnakeassefa28@gmail.com>
2022-09-19 16:33:05 +0300

    all update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
 
-if (process.argv.length > 2) {
-  console.log('Argument found');
-} else {
-  console.log('No argument');
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else if (process.argv.length > 2){
+  console.log('Agument found');
+}else{
+  console.log('No argument')
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const process = require('process');
-if (process.argv.length > 2) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
