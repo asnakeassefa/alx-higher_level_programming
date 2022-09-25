9e2db7157a99743004dacc5d42163d0acc6683d1
asnakeassefa <asnakeassefa28@gmail.com>
2022-09-25 11:05:49 +0300

    square with char

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -45,10 +45,13 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        '''
+            returns with character
+        '''
         if self.__size == 0:
             print('')
         else: 
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print('#',end=' ')
+                    print('#',end='')
                 print('')
