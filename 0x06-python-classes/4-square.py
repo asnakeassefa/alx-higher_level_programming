417473f5e667014acbab1a725813a5669bf16286
asnakeassefa <asnakeassefa28@gmail.com>
2022-09-25 10:42:55 +0300

    set and get

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+    class Square is initialize size
+'''
+
+class Square:
+    '''
+    initialize a size
+    '''
+    def __init__(self, size=0):
+        '''
+        Args:
+            size(int): square
+        '''
+        if not type(size) is int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise  ValueError('size must be >= 0')
+        self.__size = size
+
+
+    def size(self):
+        return self.__size
+    
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
