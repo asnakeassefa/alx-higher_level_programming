e5a3b3d8df8c11a288226ddde5a9be89022788b7
asnakeassefa <asnakeassefa28@gmail.com>
2022-10-07 18:48:56 +0300

    3 in one

--- 0x04-python-more_data_structures/10-best_score.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    best_key = list(a_dictionary.keys())[0]
-    for k, v in a_dictionary.items():
-        if (a_dictionary[best_key] < v):
-            best_key = k
-    return best_key
