e92ba666e84fd736530f9ed60f1f6e125802a4d0
asnakeassefa <asnakeassefa28@gmail.com>
2022-10-07 18:35:25 +0300

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,5 +6,7 @@ def search_replace(my_list, search, replace):
     for x in my_list:
         if val.get(x):
             x = replace
+        if val.get(x) == 0 and replace == 0:
+            x = replace
         new_list.append(x)
     return new_list
